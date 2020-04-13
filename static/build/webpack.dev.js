dfc25807f667f24fd9fc38db1422d7cf6c9d6e31
taotao <oncwnuKJY6G8EpTfb-s3tMZD7Ers@git.weixin.qq.com>
2020-04-13 20:18:44 +0800

    fix

---
+++ static/build/webpack.dev.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+const devConfig = {
+    mode: 'development',
+    // devtool: 'none',//关闭开发模式 sourceMap
+    devtool: 'cheap-module-eval-source-map',
+    //webpack-dev-server
+    devServer: {
+        contentBase: './dist',
+        open: false, //自动打开游览器
+        port: 8080,
+        proxy: {
+            '/': "http: //localhost: 8080"
+        },
+        //热模块更新
+        hot: true,
+        hotOnly: true
+    },
+    plugins: [
+        //热模块更新
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    // Tree Shaking
+    optimization: {
+        usedExports: true
+    }
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ static/src/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    console.log('sdsds');
+    console.log("sdsdsdsds");
+    console.log('sdsdsdsds');
+});
+
+console.log("sdsdssds")
