4b23d0e407f208d9003356a89ca44bfee42e6fa6
taotao <oncwnuKJY6G8EpTfb-s3tMZD7Ers@git.weixin.qq.com>
2020-05-27 19:52:09 +0800

    fix

--- services/employee.go
+++ services/employee.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"errors"
 	"youtuerp/models"
+	"youtuerp/redis"
 	"youtuerp/repositories"
 )
 
@@ -22,48 +24,74 @@ type EmployeeService struct {
 	BaseService
 }
 
-func (e *EmployeeService) Delete(id uint) error {
-	return e.repo.Delete(id)
+func (e EmployeeService) Delete(id uint) error {
+	err := e.repo.Delete(id)
+	if err != nil {
+		return err
+	}
+	return redis.NewRedis().SRemove(models.User{}.TableName(), id)
 }
 
-func (e *EmployeeService) First(id uint) (*models.Employee, error) {
+func (e EmployeeService) First(id uint) (*models.Employee, error) {
 	return e.repo.First(id)
 }
 
-func (e *EmployeeService) Create(employee models.Employee) (models.Employee, error) {
-	return e.repo.Create(employee)
+func (e EmployeeService) Create(employee models.Employee) (models.Employee, error) {
+	data, err := e.repo.Create(employee)
+	if err != nil {
+		return data, err
+	}
+	e.SaveRedisData(data)
+	return data, nil
 }
 
-func (e *EmployeeService) Find(per, page uint, filter map[string]interface{}, selectKeys []string, order []string, isCount bool) ([]models.ResultEmployee, uint, error) {
+func (e EmployeeService) Find(per, page uint, filter map[string]interface{}, selectKeys []string, order []string, isCount bool) ([]models.ResultEmployee, uint, error) {
 	return e.repo.Find(per, page, filter, selectKeys, order, isCount)
 }
 
 func (e *EmployeeService) UpdateRecord(user *models.Employee, employee models.Employee) error {
-	//validate := NewValidatorService(user)
-	//errs, _ := validate.HandlerError("zh-CN")
-	//if len(errs) > 0 {
-	//	return errors.New("保存失败")
-	//}
-	return e.repo.UpdateRecordByModel(user, employee)
+	validate := NewValidatorService(employee)
+	if message := validate.ResultError("zh-CN"); message != "" {
+		return errors.New(message)
+	}
+	err := e.repo.UpdateRecordByModel(user, employee)
+	if err != nil {
+		return err
+	}
+	e.SaveRedisData(*user)
+	return nil
 }
 
-func (e *EmployeeService) UpdateColumn(user *models.Employee, updateColumn map[string]interface{}) error {
-	return e.repo.UpdateColumn(user, updateColumn)
+func (e EmployeeService) UpdateColumn(user *models.Employee, updateColumn map[string]interface{}) error {
+	err := e.repo.UpdateColumn(user, updateColumn)
+	if err != nil {
+		return err
+	}
+	e.SaveRedisData(*user)
+	return nil
 }
 
-func (e *EmployeeService) FirstByPhoneAndEmail(phone string, email string) (*models.Employee, error) {
+func (e EmployeeService) FirstByPhoneAndEmail(phone string, email string) (*models.Employee, error) {
 	return e.repo.FirstByPhoneAndEmail(phone, email)
 }
 
-func (e *EmployeeService) UpdatePassword(user *models.Employee, password string) error {
+func (e EmployeeService) UpdatePassword(user *models.Employee, password string) error {
 	updateColumn := map[string]interface{}{
 		"encrypted_password": password,
 	}
 	return e.repo.UpdateColumn(user, updateColumn)
 }
 
-func (e *EmployeeService) FirstByPhoneOrEmail(account string) (*models.Employee, error) {
+func (e EmployeeService) FirstByPhoneOrEmail(account string) (*models.Employee, error) {
 	return e.repo.FirstByPhoneOrEmail(account)
+}
+
+func (e EmployeeService) SaveRedisData(result models.Employee) {
+	redis.HSetValue(models.User{}.TableName(), result.ID, map[string]interface{}{
+		"id":              result.ID,
+		"name":            result.Name,
+		"user_company_id": result.UserCompanyId,
+	})
 }
 
 func NewEmployeeService() IEmployeeService {
